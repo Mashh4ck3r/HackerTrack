39818e36e734cf6e703b5813148792b119291174
Alt-Dhanush321 <dhanush321alt@gmail.com>
2023-08-21 19:20:17 +0530

    program page 90% over

--- src/components/Programs/ProgramCard.jsx
+++ src/components/Programs/ProgramCard.jsx
@@ -18,33 +18,33 @@ function ProgramCard() {
     const items = data?.items
 
     return (
-        <div className='flex flex-wrap gap-5'>
+        <div className='grid grid-cols-4 gap-y-12'>
             {
                 items?.map((item, i) => (
-                    <div className='text-white program-card w-60 h-80 flex gap-1 flex-col justify-between  bg-black' key={i}>
-                        <div className="prog-head border-b-2 border-b-secondary flex justify-between items-center">
-                            <div className="prog-brand flex overflow-hidden flex-nowrap items-center gap-1">
+                    <div className='text-white program-card w-60 h-80 flex gap-1 flex-col justify-between  bg-black rounded-md dark-shadow' key={i}>
+                        <div className="py-2 px-3 prog-head border-b-[1px] border-b-secondary flex justify-between items-center">
+                            <div className="prog-brand flex overflow-hidden flex-nowrap items-center gap-2">
                                 <img src={item.business_unit.logo.url || defaultImg} alt={item.business_unit.logo.original_name} className="brand rounded-full w-9 bg-white" />
-                                <h1 className=''>{item.business_unit.name}</h1>
+                                <h1 className='flex flex-nowrap'>{item.business_unit.name.slice(0, 16)}</h1>
                             </div>
                             <AiOutlineStar />
                         </div>
-                        <div className="prog-body border-b-2 border-b-secondary flex-1">
-                            <div className="info">
-                                <h1>{item.title}</h1>
-                                <p>{item.business_unit.description.slice(0, 40) + "...."}</p>
+                        <div className="prog-body py-2 px-3 border-b-[1px] border-b-secondary flex-1">
+                            <div className="info flex flex-col gap-4">
+                                <h1 className=' font-semibold'>{item.title}</h1>
+                                <p className='text-xs pb-5'>{item.business_unit.description.slice(0, 60) + "...."}</p>
                             </div>
-                            <div className="prog-reward">
+                            <div className="prog-reward text-secondary">
                                 <p>
                                     {
-                                        "$" + item.bounty_reward_min + "-" + item.bounty_reward_max
+                                        "$" + item.bounty_reward_min + " - $" + item.bounty_reward_max
                                     }
                                 </p>
-                                <p>{item.type}</p>
+                                <p className='text-white'>{item.type}</p>
                             </div>
                         </div>
                         <div className="prog-footer flex justify-center items-center p-3">
-                            <button className=' rounded-md px-4 py-2 border-secondary border-2'>see details </button>
+                            <button className='hover:bg-secondary rounded-md px-4 py-1 border-secondary border-2'>see details </button>
                         </div>
                     </div>
                 ))
