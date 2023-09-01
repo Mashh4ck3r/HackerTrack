cbbc20e0c0355bb6d0fe0c452ef7e9b03ce26b5e
Alt-Dhanush321 <dhanush321alt@gmail.com>
2023-09-01 20:33:41 +0530

    navbar dummy commit

--- client/src/components/navbra/Navbar.jsx
+++ client/src/components/navbra/Navbar.jsx
@@ -18,7 +18,7 @@ function Navbar() {
                 <li className="list-item nav-item"><Link to='/url-check'>Quick Check </Link></li>
                 <li className="list-item nav-item"><a href='http://localhost/dvwa/' target='_blank'>Skill Test </a></li>
                 <li className="list-item nav-item"><Link to='/login'>
-                    <button className='btn rounded-md bg-secondary px-5 py-2'>Get Started</button>
+                    <button className='btn rounded bg-secondary px-5 py-2'>Get Started</button>
                  </Link></li>
             </ul>
         </nav>
